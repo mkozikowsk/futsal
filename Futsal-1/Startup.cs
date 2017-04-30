cceb070b27c15b12d1a8ac27909f2b0c8a6c2967
Marcin Kozikowski <Marcin Kozikowski>
2017-04-30 13:29:48 +0200

    Dodaj pliki projektów.

---
+++ Futsal-1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Futsal_1.Startup))]
+namespace Futsal_1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
