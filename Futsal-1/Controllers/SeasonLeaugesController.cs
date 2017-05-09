d20ac022a4298111976c8b9af4b64c88b13d82da
Pixel <marcino1104@gmail.com>
2017-05-09 15:18:19 +0200

    Database migration

--- Futsal-1/Controllers/SeasonLeaugesController.cs
+++ Futsal-1/Controllers/SeasonLeaugesController.cs
@@ -49,7 +49,7 @@ namespace Futsal_1.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,Status,SeasonId,TeamId")] SeasonLeauge seasonLeauge)
+        public ActionResult Create([Bind(Include = "Id,Status,SeasonId")] SeasonLeauge seasonLeauge)
         {
             if (ModelState.IsValid)
             {
@@ -83,7 +83,7 @@ namespace Futsal_1.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,Status,SeasonId,TeamId")] SeasonLeauge seasonLeauge)
+        public ActionResult Edit([Bind(Include = "Id,Status,SeasonId")] SeasonLeauge seasonLeauge)
         {
             if (ModelState.IsValid)
             {
